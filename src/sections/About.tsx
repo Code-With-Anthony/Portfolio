34728a64db07b628249d9208dbf50c26ff09a7c8
Anthony Dourado <anthony.dourado@thegatewaycorp.co.in>
2025-02-18 16:36:19 +0530

    About Section completion and other section changes

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,6 +1,6 @@
+"use client";
 import { Card } from "@/components/Card";
 import { SectionHeader } from "@/components/SectionHeader";
-import StarIcon from "@/assets/icons/star.svg";
 import bookImage from "@/assets/images/book-cover.png";
 import Image from "next/image";
 import JavascriptIcon from "@/assets/icons/square-js.svg";
@@ -9,9 +9,9 @@ import CSS3Icon from "@/assets/icons/css3.svg";
 import ChromeIcon from "@/assets/icons/chrome.svg";
 import GithubIcon from "@/assets/icons/github.svg";
 import ReactIcon from "@/assets/icons/react.svg";
-import { TechIcon } from "@/components/TechIcon";
-import mapImage from "@/assets/images/map.png";
-import smileEmoji from "@/assets/images/memoji-smile.png";
+import { CardHeader } from "@/components/CardHeader";
+import { ToolboxItems } from "@/components/ToolboxItems";
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 
 const toolBoxItems = [
   {
@@ -44,92 +44,135 @@ const hobbies = [
   {
     title: "Painting",
     emoji: "🎨",
+    left: "5%",
+    top: "5%",
   },
   {
     title: "Photography",
     emoji: "📷",
-  },
-  {
-    title: "Gaming",
-    emoji: "🎮",
+    left: "50%",
+    top: "5%",
   },
   {
     title: "Hiking",
     emoji: "🥾",
+    left: "35%",
+    top: "40%",
+  },
+  {
+    title: "Gaming",
+    emoji: "🎮",
+    left: "10%",
+    top: "35%",
   },
   {
     title: "Music",
     emoji: "🎶",
+    left: "70%",
+    top: "45%",
   },
   {
     title: "Fitness",
     emoji: "🏋",
+    left: "5%",
+    top: "65%",
   },
   {
     title: "Reading",
     emoji: "📚",
+    left: "45%",
+    top: "70%",
   },
 ];
 
+const mapContainerStyle = {
+  width: "100%",
+  height: "100%",
+};
+
+const center = {
+  lat: 23.022505,
+  lng: 72.571365,
+};
+
 export const AboutSection = () => {
   return (
-    <div className="py-20">
+    <div className="py-20 lg:py-28">
       <div className="container">
         <SectionHeader
           eyebrow="About Me"
           title="A Glimpse into my world"
           description="Learn more about who I am, What I do, and what inspires me."
         />
-        <div className="mt-20">
-          <Card>
-            <div>
-              <div className="inline-flex items-center gap-2">
-                <StarIcon className="size-9 text-emerald-300" />
-                <h3 className="font-serif text-3xl">My Reads</h3>
+        <div className="mt-20 flex flex-col gap-8">
+          <div className="grid grid-cols-1 gap-8 md:grid-cols-5 lg:grid-cols-3">
+            <Card className="h-[320px] md:col-span-2 lg:col-span-1">
+              <CardHeader
+                title="My Reads"
+                description="Explore the book shaping my perspectives"
+              />
+              <div className="w-40 mx-auto mt-2 md:mt-0">
+                <Image src={bookImage} alt="Book cover" />
               </div>
-              <p className="text-sm text-white/60">
-                Explore the book shaping my perspectives
-              </p>
-            </div>
-            <Image src={bookImage} alt="Book cover" />
-          </Card>
-          <Card>
-            <div>
-              <StarIcon />
-              <h3>My Toolbox</h3>
-              <p>
-                Explore the technologies and tools I use to craft exceptional
-                digital experiences.
-              </p>
-            </div>
-            <div>
-              {toolBoxItems.map((item) => (
-                <div key={item.title}>
-                  <TechIcon component={item.iconType} />
-                  <span>{item.title}</span>
-                </div>
-              ))}
-            </div>
-          </Card>
-          <Card>
-            <div>
-              <StarIcon />
-              <h3>Beyond the Code</h3>
-              <p>Explore my interest and hobbies beyond the digital realm.</p>
-            </div>
-            <div>
+            </Card>
+            <Card className="h-[320px] md:col-span-3 lg:col-span-2">
+              <CardHeader
+                title="My Toolbox"
+                description="Explore the technologies and tools I use to craft exceptional digital experiences."
+                className=""
+              />
+
+              <ToolboxItems items={toolBoxItems} className="" />
+              <ToolboxItems
+                items={toolBoxItems}
+                className="mt-6"
+                itemsWrapperClassName="-translate-x-1/2"
+              />
+            </Card>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-5 lg:grid-cols-3 gap-8">
+          <Card className="h-[320px] p-0 flex flex-col md:col-span-3 lg:col-span-2">
+            <CardHeader
+              title="Beyond the Code"
+              description="Explore my interest and hobbies beyond the digital realm."
+              className="px-6 py-6"
+            />
+            <div className="relative flex-1">
               {hobbies.map((hobby) => (
-                <div key={hobby.title}>
-                  <span>{hobby.title}</span>
+                <div
+                  key={hobby.title}
+                  className="inline-flex items-center gap-2 px-6 bg-gradient-to-r from-emerald-300 to-sky-400 rounded-full py-1.5 absolute"
+                  style={{ left: hobby.left, top: hobby.top }}
+                >
+                  <span className="font-medium text-gray-950">
+                    {hobby.title}
+                  </span>
                   <span>{hobby.emoji}</span>
                 </div>
               ))}
             </div>
           </Card>
-          <Card>
-            <Image src={mapImage} alt="map" />
-            <Image src={smileEmoji} alt="smiling emoji" />
+          <Card className="h-[320px] p-0 md:col-span-2 lg:col-span-1">
+            {/* <Image
+              src={mapImage}
+              alt="map"
+              className="h-full w-full object-cover"
+            />
+            <Image src={smileEmoji} alt="smiling emoji" /> */}
+            <LoadScript
+              googleMapsApiKey={process.env.NEXT_PUBLIC_MAP_API_KEY || ""}
+            >
+              {/* Google Map Component */}
+              <GoogleMap
+                mapContainerStyle={mapContainerStyle}
+                center={center}
+                zoom={14}
+              >
+                <Marker position={center} />
+              </GoogleMap>
+            </LoadScript>
           </Card>
+          </div>
         </div>
       </div>
     </div>
