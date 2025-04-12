748222c60b9908294346b841e15efb283623c0b0
Anthony Dourado <anthonydourado111@gmail.com>
2025-04-12 23:25:58 +0530

    Added Calendar Booking

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,19 +1,7 @@
 import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
 import grainImage from "@/assets/images/grain.jpg";
-import { Card } from "@/components/Card";
-import { useState } from "react";
 
 export const ContactSection = () => {
-  const [openModal, setOpenModal] = useState(false);
-
-  const handleOpenCalenderBooking = () => {
-    setOpenModal(true);
-  };
-
-  const handleCloseCalenderBooking = () => {
-    setOpenModal(false);
-  };
-
   return (
     <div className="py-16 pt-12 lg:py-24 lg:pt-20">
       <div className="container">
@@ -30,45 +18,26 @@ export const ContactSection = () => {
                 Let&apos;s create something amazing together
               </h2>
               <p className="text-sm md:text-base mt-2">
-                Ready to bring your next project to life? Let&apos;s connect and
-                discuss. I can help you achieve your goals.
+                Ready to bring your next project to life? Let&apos;s connect
+                over Google Meet & discuss. I can help you achieve your goals.
               </p>
             </div>
             <div>
               <button
-                // onClick={() =>
-                //   window.open("https://calendly.com/anthonydourado111")
-                // }
-                onClick={handleOpenCalenderBooking}
+                onClick={() =>
+                  window.open(
+                    "https://calendar.google.com/calendar/appointments/schedules/AcZssZ2-2pXiG0lHpwA7HDEMeez22MRHDB8ldVcOHLZym1f6Yl4VFls5sn61DAV_pYMKjBE9PCASOMCp?gv=true",
+                    "_blank"
+                  )
+                }
                 className="text-white bg-gray-900 inline-flex items-center px-6 h-12 rounded-xl gap-2 w-max border border-gray-950"
               >
-                <span className="font-semibold">Contact Me</span>
+                <span className="font-semibold">Book Calendar</span>
                 <ArrowUpRightIcon className="size-4" />
               </button>
             </div>
           </div>
         </div>
-
-        {/* modal */}
-        {openModal && (
-          <div
-            className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50"
-            onClick={handleCloseCalenderBooking}
-          >
-            <Card
-              className="bg-white rounded-lg p-6 text-black"
-              onClick={(e) => e.stopPropagation()}
-            >
-              <h2 className="text-xl font-semibold mb-4">
-                Schedule Meeting with me
-              </h2>
-              <p>
-                Welcome to my scheduling page. Please follow the instructions to
-                add an event to my calendar.
-              </p>
-            </Card>
-          </div>
-        )}
       </div>
     </div>
   );
